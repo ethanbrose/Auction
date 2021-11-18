4ef1f2143ffd32b22a20dba6e903e2adb3a2c897
Aaron Cho <aars.cho10@gmail.com>
2021-11-18 02:03:28 -0800

    Added OG designs and tested them + quality of life and polishing
    Implemented:
    - More specificity when setting the duration of the auction (now 4 parameters for weeks, days, hours, & minutes) in the constructor.
    - Edited setter accordingly.
    - Original function to confirm a user's bid larger than 20% of the current highest bid. (used two methods to accomplish this)
    - Original function to have the seller stop (pull) the auction.
    - Original design to have users provide a less than 150 character description.
    - Added comments just making clear what is what.
    
    Tested:
    - Bounds of setting the duration of the auction in the constructor.
    - Description size. (this one was pretty cool to test.)
    - Pull Auction()
    - The setters as well
    
    Need to try to test again:
    - Bid functions. The time ran out when I made the first contract :/ I'll try this again.

--- AuctionSystem.sol
+++ AuctionSystem.sol
@@ -15,33 +15,43 @@ contract AuctionSystem {
     
     IERC721 public nft;
     uint256 public nftID;
+    string public description;
     
     uint256 public highestBid = 0;
     address public highestBidder = address(0);
     
     
-    //The constructor initializes many variables
-    constructor (uint256 numDays, uint256 startingPrice) {
+    //The constructor initializes many variables. I guess it is OG design to choose the auction duration. There are a lot of choices of to make about the duration now.
+    constructor (uint256 numWeeks, uint256 numDays, uint256 numHours, uint256 numMins, uint256 startingPrice) {
+        require (startingPrice > 0 && numWeeks >= 0 && numDays >= 0 && numHours >= 0 && numMins >= 0, "Error: Invalid parameter(s). Price must be greater than 0 and time intervals can be specified as 0 or greater.");
         highestBid = startingPrice;
         owner = payable (msg.sender);
+        duration = block.timestamp + (numWeeks * 1 weeks);
         duration = block.timestamp + (numDays * 1 days);
+        duration = block.timestamp + (numHours * 1 hours);
+        duration = block.timestamp + (numMins * 1 minutes);
     }
     
-    function setNFT (IERC721 _nft, uint256 _nftID) public {
-        nft = _nft;
-        nftID = _nftID;
-    }
     
-    //regular old bid function
+    
+    //regular old bid function. IMPLEMENTS OG DESIGN: adding 10 mins when a bid is placed AND the 20% higher than current price confirmation. Solved by just having two methods to bid.
     function bid (uint256 curBid) public notEnded {
-        if (curBid > highestBid) {
-            highestBid = curBid;
-            highestBidder = msg.sender;
-        }
+        require ((highestBid + (highestBid/5)) > curBid, "Quite the large bid there. If you are sure about bidding more than 20% higher than the current highest price, please use the bidBigger method.");
+        require (curBid > highestBid, "Error: your bid is not smaller than the current highest!");
+        highestBid = curBid;
+        highestBidder = msg.sender;
+        duration = block.timestamp + (10 minutes);
+    }
+    
+    function bidBigger (uint256 curBid) public notEnded {
+        require (curBid > highestBid, "Error: your bid is not smaller than the current highest!");
+        highestBid = curBid;
+        highestBidder = msg.sender;
+        duration = block.timestamp + (10 minutes);
     }
     
     
-    //if called, this function automatically bids a bid 5% larger than the current highest bid.
+    //OG DESIGN METHOD: if called, this function automatically bids a bid 5% larger than the current highest bid.
     function quickBid () public notEnded {
             highestBid = highestBid + (highestBid/20);
             highestBidder = msg.sender;
@@ -61,10 +71,21 @@ contract AuctionSystem {
     
     function winNow () public payable {
         nft.safeTransferFrom (address(this), highestBidder, nftID);
+        
+        emit Win (highestBidder, highestBid);
+    }
+    
+    //OG DESIGN FEATURE
+    function pullAuction () public {
+        require (msg.sender == owner, "Error: You may not do that.");
+        delete nft;
+        delete nftID;
+        setDuration (0, 0, 0, 0);
+        setHighestBidder(address(0));
     }
     
     
-    //modifiers
+    //modifier section
     
     modifier notEnded () {
         require (block.timestamp < duration, "This auction has ended.");
@@ -75,6 +96,7 @@ contract AuctionSystem {
         require (block.timestamp > duration, "This auction has not ended.");
         _;
     }
+    
     
     //getter section
     
@@ -98,10 +120,28 @@ contract AuctionSystem {
         return highestBid;
     }
     
-    // setters (but only for the duration because it won't let me win the auction unless the duration is 0)
     
-    function setDuration (uint256 _duration) public {
-        duration = _duration;
+    // setter section
+    
+    function setDuration (uint256 numWeeks, uint256 numDays, uint256 numHours, uint256 numMins) public {
+        duration = block.timestamp + (numWeeks * 1 weeks);
+        duration = block.timestamp + (numDays * 1 days);
+        duration = block.timestamp + (numHours * 1 hours);
+        duration = block.timestamp + (numMins * 1 minutes);
+    }
+    
+    function setHighestBidder (address _highestBidder) public {
+        highestBidder = _highestBidder;
+    }
+    
+    function setDescription (string memory _description) public {
+        require (bytes(_description).length <= 151 && bytes(_description).length > 0, "Error: Description must be within 0-150 characters!");
+        description = _description;
+    }
+    
+    function setNFT (IERC721 _nft, uint256 _nftID) public {
+        nft = _nft;
+        nftID = _nftID;
     }
     
 }
